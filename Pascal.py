03600251218e26ea53bb64d75e759ddf52ce7556
shubhranshu789 <66314743+shubhranshu789@users.noreply.github.com>
2026-01-20 09:51:33 +0530

    Initial commit

---
+++ Pascal.py
@@ -0,0 +1,11 @@
+def pascal_triangle(n):
+    for i in range(n):
+        print(' ' * (n - i), end='')
+        
+        C = 1
+        for j in range(i + 1):
+            print(C, end=' ')
+            C = C * (i - j) // (j + 1)
+        print()
+
+pascal_triangle(6)
