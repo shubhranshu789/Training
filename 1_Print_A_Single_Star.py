fa972db4b20c6f9276a0e45f2bc1edb69de971f4
shubhranshu789 <66314743+shubhranshu789@users.noreply.github.com>
2026-01-20 11:15:46 +0530

    Initial commit

---
+++ 1_Print_A_Single_Star.py
@@ -0,0 +1,13 @@
+print("*")
+
+for i in range(5):
+    print(" " * 5 , end = " ")
+
+print("*")
+
+for i in range(10):
+    print(" " * 5 , end = " ")
+
+print("*")
+
+
--- Pascal.py
+++ Pascal.py
@@ -1,11 +1,26 @@
-def pascal_triangle(n):
-    for i in range(n):
-        print(' ' * (n - i), end='')
-        
+def pascal_triangle2(n):
+    for i in range (n):
+        print(' ' * (n-i) , end = '')
+    
         C = 1
-        for j in range(i + 1):
-            print(C, end=' ')
-            C = C * (i - j) // (j + 1)
+        for j in range (i+1):
+            print(C , end = ' ')
+            C = C * (i-j) // (j+1)
         print()
 
-pascal_triangle(6)
+def pascal_triangle(n):
+    for i in range(n):
+        print(' ' * (n-i), end='')
+        
+        C = 1
+        for j in range(i+1):
+            print(C, end=' ')
+            C = C * (i-j) // (j+1)
+        print()
+
+
+
+pascal_triangle2(6)
+
+
+
